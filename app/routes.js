4cd10da9268166a891a2d06042ece5ee7f01b561
Yovi Budirahardjo <yovi@yovib.com.au>
2015-10-06 02:00:27 +1100

    Refactor

--- app/routes.js
+++ app/routes.js
@@ -8,30 +8,30 @@ router.post('/', (req, res) => {
 
     try {
         data = JSON.parse(req.body);
-    } catch(e) {
-        res.status(400).json({ error: 'Could not decode request: Invalid JSON format' });
-    }
 
-    if (!data || !data.payload) {
-        console.error('Invalid data');
-    } else {
-        res.json({
-            response:
-                data
-                .payload
-                .filter((item) => {
-                    return item.image &&
-                            item.drm &&
-                            item.episodeCount > 0;
-                })
-                .map((item) => {
-                    return {
-                        image: item.image.showImage,
-                        slug: item.slug,
-                        title: item.title
-                    };
-                })
-        });
+        if (data && data.payload) {
+            res.json({
+                response:
+                    data
+                    .payload
+                    .filter((item) => {
+                        return item.image &&
+                                item.drm &&
+                                item.episodeCount > 0;
+                    })
+                    .map((item) => {
+                        return {
+                            image: item.image.showImage,
+                            slug: item.slug,
+                            title: item.title
+                        };
+                    })
+            });
+        } else {
+            res.send('Invalid data');
+        }
+    } catch(err) {
+        res.status(400).json({ error: 'Could not decode request: ' + err });
     }
 });
 
